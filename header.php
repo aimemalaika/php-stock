84d6fde508bda63605f1431057f622f49781b231
aime malaika <aime@localhost.localdomain>
2017-11-13 12:02:41 -0600

    creation of future page ok

--- header.php
+++ header.php
@@ -1,25 +1,9 @@
-<!DOCTYPE HTML>
-<html>
-
-<head>
-
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Platz - Free Grid Wordpress Theme</title>
-
-<!-- Behavioral Meta Data -->
-	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="icon" type="image/png" href="img/small-logo-01.png">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,900,900italic,700italic,700,500italic,400italic,500,300italic,300' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-	<link href='style.css' rel='stylesheet' type='text/css'>
-
-
-
-</head>
-
-<body>
-
+<?php
+  session_start();
+    if (!isset($_SESSION['username']) AND !isset($_SESSION['shopname'])) {
+        header('Location: login');
+    }
+?>
 <a name="ancre"></a>
 
 <!-- CACHE -->
@@ -29,11 +13,9 @@
 
 <div id="wrapper-header">
 	<div id="main-header" class="object">
-		<div class="logo"><img src="img/logo-burst.png" alt="logo platz" height="38" width="90"></div>
+		<div class="logo" style="margin-top:7px;"><strong>Company name</stong></div>
         <div id="main_tip_search">
-			<form>
-				<input type="text" name="search" id="tip_search_input" list="search" autocomplete=off required>
-			</form>
+      <a class="ret" style="text-decoration: none;color:gray;" href="home">go to main menu</a>
 		</div>
         <div id="stripes"></div>
     </div>
@@ -42,94 +24,18 @@
 <!-- NAVBAR -->
 
 <div id="wrapper-navbar">
-		<div class="navbar object">
-    		<div id="wrapper-sorting">
-            <div id="wrapper-title-1">
-            <div class="top-rated object">Top-rated</div>
-            	<div id="fleche-nav-1"></div>
-    		</div>
-
-            <div id="wrapper-title-2">
-            <a href="#"><div class="recent object">Recent</div></a>
-                <div id="fleche-nav-2"></div>
-    		</div>
-
-            <div id="wrapper-title-3">
-            <a href="#"><div class="oldies object">Oldies</div></a>
-                <div id="fleche-nav-3"></div>
-    		</div>
-            </div>
-            <div id="wrapper-bouton-icon">
-            	<div id="bouton-ai"><img src="img/icon-ai.svg" alt="illustrator" title="Illustrator" height="28" width="28"></div>
-            	<div id="bouton-psd"><img src="img/icon-psd.svg" alt="photoshop" title="Photoshop" height="28" width="28"></div>
-            	<div id="bouton-theme"><img src="img/icon-themes.svg" alt="theme" title="Theme" height="28" width="28"></div>
-            	<div id="bouton-font"><img src="img/icon-font.svg" alt="font" title="Font" height="28" width="28"></div>
-            	<div id="bouton-photo"><img src="img/icon-photo.svg" alt="photo" title="Photo" height="28" width="28"></div>
-            	<div id="bouton-premium"><img src="img/icon-premium.svg" alt="premium" title="Premium" height="28" width="28"></div>
-			</div>
-    	</div>
     </div>
     <div id="wrapper-navbar" style="opacity: 1; height: 60px;">
 		<div class="navbar object">
     		<div id="wrapper-sorting">
             <div id="wrapper-title-1">
-            <div class="top-rated object">Top-rated</div>
-            	<div id="fleche-nav-1"></div>
-    		</div>
-
-            <div id="wrapper-title-2">
-            <a href="#"><div class="recent object">Recent</div></a>
-                <div id="fleche-nav-2"></div>
-    		</div>
-
-            <div id="wrapper-title-3">
-            <a href="#"><div class="oldies object">Oldies</div></a>
-                <div id="fleche-nav-3"></div>
+            <div class="top-rated object">pppppp</div>
     		</div>
             </div>
             <div id="wrapper-bouton-icon">
-            	<div id="bouton-ai"><img src="img/icon-ai.svg" alt="illustrator" title="Illustrator" width="28" height="28"></div>
-            	<div id="bouton-psd"><img src="img/icon-psd.svg" alt="photoshop" title="Photoshop" width="28" height="28"></div>
-            	<div id="bouton-theme"><img src="img/icon-themes.svg" alt="theme" title="Theme" width="28" height="28"></div>
-            	<div id="bouton-font"><img src="img/icon-font.svg" alt="font" title="Font" width="28" height="28"></div>
-            	<div id="bouton-photo"><img src="img/icon-photo.svg" alt="photo" title="Photo" width="28" height="28"></div>
-            	<div id="bouton-premium"><img src="img/icon-premium.svg" alt="premium" title="Premium" width="28" height="28"></div>
-			</div>
+              <div style="float:right" class="top-rated object"><a href="">Log out</a></div>
+              <div style="float:right" class="top-rated object"><a href="">Add a team member</a></div>
+              <div style="float:right" class="top-rated object"> hi name!</div>
+			      </div>
     	</div>
     </div>
-
-
-
-<!-- SCRIPT -->
-
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
-  <script type="text/javascript" src="js/jquery.localScroll.min.js"></script>
-  <script type="text/javascript" src="js/jquery-animate-css-rotate-scale.js"></script>
-	<script type="text/javascript" src="js/fastclick.min.js"></script>
-	<script type="text/javascript" src="js/jquery.animate-colors-min.js"></script>
-	<script type="text/javascript" src="js/jquery.animate-shadow-min.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
-	<script>
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function') {
-		window.onload = func;
-	} else {
-		window.onload = function() {
-			if (oldonload) {
-				oldonload();
-			}
-			func();
-		}
-	}
-}
-addLoadEvent(preloader);
-
-</script>
-
-
-</body>
-
-
-</html>
---
+++ purchase-credit.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Purchase credit</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="style/style.css">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link rel="icon" type="image/png" href="img/small-logo-01.png">
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,900,900italic,700italic,700,500italic,400italic,500,300italic,300' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+<link href='style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+<div id="wrapper">
+  <div id="content" style="border-bottom: 7px solid #F4F4F4;">
+    <div id="sidebar">
+      <ul>
+          <h2>Client list</h2>
+          <ul>
+            <li><i style="font-size: 17px;color: #60ff00;" class="fa fa-ellipsis-h" aria-hidden="true"></i> <a href="#">client one</a></li>
+            <li><i style="font-size: 17px;color: #60ff00;" class="fa fa-ellipsis-h" aria-hidden="true"></i> <a href="#">client one</a></li>
+            <li><i style="font-size: 17px;color: #60ff00;" class="fa fa-ellipsis-h" aria-hidden="true"></i> <a href="#">client one</a></li>
+            <li><i style="font-size: 17px;color: #60ff00;" class="fa fa-ellipsis-h" aria-hidden="true"></i> <a href="#">client one</a></li>
+            <li><i style="font-size: 17px;color: #60ff00;" class="fa fa-plus-square-o" aria-hidden="true"></i> <a href="#">Add purchase credit</a></li>
+          </ul>
+        </ul>
+    </div>
+    <!-- end #sidebar -->
+    <div id="blog">
+      <center><h2 style="margin-top: 17px;">List of available product</h2></center>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+      <div class='package'>
+        <div class='name'>Iphone 6</div>
+        <div class='trial'>100 000 frw</div>
+        <hr>
+        <div class='trial'>Description</div>
+        <hr>
+        <br>
+        <div style="margin-bottom: -7px;max-height: 48px;overflow: hidden;">
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+            babababbaba abbababab abbababa abba abba ba abbaba ba aba ba aba ba ab
+        </div>
+        <div class='trial'>Available stock</div>
+        <hr>
+        <br>
+        <center><h4 style="margin-top: -4px;color:black">120 iphone(s)</h4></center>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
+<!-- SCRIPT -->
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+  <script type="text/javascript" src="js/jquery.localScroll.min.js"></script>
+  <script type="text/javascript" src="js/jquery-animate-css-rotate-scale.js"></script>
+	<script type="text/javascript" src="js/fastclick.min.js"></script>
+	<script type="text/javascript" src="js/jquery.animate-colors-min.js"></script>
+	<script type="text/javascript" src="js/jquery.animate-shadow-min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<script>
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+addLoadEvent(preloader);
+
+</script>
+</body>
+</html>
